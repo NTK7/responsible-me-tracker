94a6dafacb1d8520c779a1d42789eba8ce2af88e
Nazhim <nazhimkalam@gmail.com>
2021-09-07 09:18:20 +0530

    deployed

--- src/components/processing/Processing.js
+++ src/components/processing/Processing.js
@@ -1,10 +1,12 @@
+import { CircularProgress } from "@material-ui/core";
 import styled from "styled-components";
 
 const Processing = () => {
   return (
     <Container>
-      <img src="/extra/loading.gif" alt="" />
-      <p>Processing the data, please hold on ...</p>
+      {/* <img src="/extra/loading.gif" alt="" />
+      <p>Processing the data, please hold on ...</p> */}
+      <CircularProgress color="secondary" size={100}/>
     </Container>
   );
 };
@@ -18,20 +20,21 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   padding: 1pc;
-  > img {
+  height: 80vh;
+  /* > img {
     object-fit: contain;
     width: 150px;
   }
   > p {
     text-align: center;
-  }
+  } */
   @media screen and (max-width: 500px) {
     margin: 1pc 0;
-    > img {
+    /* > img {
       width: 100px;
     }
     > p {
       padding: 0.5pc;
-    }
+    } */
   }
 `;
